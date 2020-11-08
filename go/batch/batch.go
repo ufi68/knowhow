60995ea277dd8b4e698017a4eb7c0aad7ecdf4dd
Udo Fischer <Udo.Fischer13@gmail.com>
2020-11-08 19:25:37 +0100

    batch

---
+++ go/batch/batch.go
@@ -0,0 +1,16 @@
+// Golang program to get current date and time
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	// get date and time and store in variable on left
+	currentTime := time.Now()
+
+	// printing the date and time in string format
+	fmt.Println("Current Time in String: ", currentTime.String())
+}
